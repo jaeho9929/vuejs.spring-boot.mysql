ca5addf842f0501258c3932b950570c2eea2dd77
J. H. Jeong <jaeho9929@gmail.com>
2021-12-22 21:41:02 +0900

    Profile configuration
    - dev/prod profile (default to "dev")
    - datasource configuration

--- src/test/java/com/taskagile/TaskAgileApplicationTests.java
+++ src/test/java/com/taskagile/TaskAgileApplicationTests.java
@@ -1,13 +1,32 @@
 package com.taskagile;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class TaskAgileApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Autowired
+  Environment environment;
+
+  @Test
+  public void getProfile_checkContext_ShouldReturnDev() {
+    assertThat(environment.getProperty("spring.profiles.active"))
+    .isEqualTo("dev");
+  }
+
+  @Test
+  public void getProfile_checkContext_shouldReturnProd() {
+    assertThat(environment.getProperty("spring.profiles.active"))
+    .isEqualTo("prod");
+  }
+
+	// @Test
+	// public void contextLoads() {
+	// }
 
 }
